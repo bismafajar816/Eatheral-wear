879e0bb4effd539ce0789c68fd47611f7dd10cb7
Bisma Fajar <120157054+bismafajar816@users.noreply.github.com>
2024-12-24 21:41:46 +0500

    Update server.js  allowed only deployed app

--- server/server.js
+++ server/server.js
@@ -70,11 +70,22 @@ const app = express();
 //   })
 // );
 
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      //"http://localhost:5173", // For local development (optional if you're testing locally)
+      "https://my-mern-project-frontend.vercel.app", // Your deployed frontend URL
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    credentials: true, // Allow cookies to be sent with requests
   })
 );
 
